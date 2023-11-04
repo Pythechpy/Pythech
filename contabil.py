b5500030b116be66ee29b8b1dbbffe59351dfba7
Pythechpy <149851520+Pythechpy@users.noreply.github.com>
2023-11-03 23:34:19 -0300

    Add files via upload

---
+++ contabil.py
@@ -0,0 +1,9 @@
+vendas = 12000 
+
+custos = 5000
+
+resultado = vendas - custos
+
+print("o reultado do período é de:", resultado)
+print(vendas)
+
---
+++ uso de lista.py
@@ -0,0 +1,8 @@
+listadeprodutos = ["medicamento", "ração", "suplemento", "shampoo"]
+for produto in listadeprodutos:
+    print(produto)
+listadeprecos = [200, 180, 250, 100]
+for preco in listadeprecos:  
+    print(preco)
+    
+
